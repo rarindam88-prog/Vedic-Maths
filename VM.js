81b41aad7504f6db249b1183cede5f9a45481f7b
rarindam88-prog <rarindam.88@gmail.com>
2025-12-23 12:56:35 +0530

    Delete VM.js

--- VM.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#headingOne").addEventListener("click",function() {
-if (document.getElementById("collapseOne").style.display === "none"){
-document.getElementById("collapseOne").style.display = "block"
-}
-else{
-    document.getElementById("collapseOne").style.display = "none"
-}
-});
